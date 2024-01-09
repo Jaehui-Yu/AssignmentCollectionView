7cf94fe642b4ce5369c974b2bb336bd78c660946
Jaehui Yu <pkj0522@naver.com>
2024-01-09 17:21:33 +0900

    Initial Commit

---
+++ AssignmentCollectionView/City/CityCollectionViewCell.swift
@@ -0,0 +1,42 @@
+//
+//  CityCollectionViewCell.swift
+//  AssignmentCollectionView
+//
+//  Created by Jaehui Yu on 1/9/24.
+//
+
+import UIKit
+
+class CityCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var mainLabel: UILabel!
+    @IBOutlet var subLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        imageView.contentMode = .scaleToFill
+        imageView.layer.cornerRadius = imageView.frame.height / 2
+        
+        mainLabel.font = .boldSystemFont(ofSize: 16)
+        mainLabel.textAlignment = .center
+        
+        subLabel.font = .systemFont(ofSize: 12)
+        subLabel.textColor = .gray
+        subLabel.numberOfLines = 2
+        subLabel.textAlignment = .center
+    }
+    
+    func configureCell(city: City) {
+        let url = URL(string: city.city_image)
+        imageView.kf.setImage(with: url)
+        
+        mainLabel.text = "\(city.city_name) | \(city.city_english_name)"
+        
+        subLabel.text = city.city_explain
+        
+        
+    }
+
+}
